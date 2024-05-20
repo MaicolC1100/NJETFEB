a9370b3fc1c98a2a19751976c387a0c0a5879d87
Maicol C <maicolpalabras@gmail.com>
2024-05-20 15:13:00 -0500

    Primer Commit: añadir el proyecto

---
+++ src/main/java/com/aplication/jetfeb/JetfebApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.jetfeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetfebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetfebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplication/jetfeb/JetfebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.jetfeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetfebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
