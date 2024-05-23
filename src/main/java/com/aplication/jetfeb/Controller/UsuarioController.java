128a3ae8d9a0477d15d6dc1f829f8deaf4974571
CamiloRDZ <tamilo1313@hotmail.com>
2024-05-22 21:46:35 -0500

    - cambios en el registro, autenticacion - creacion de plantilla footer y header - ajustes css

--- src/main/java/com/aplication/jetfeb/Controller/UsuarioController.java
+++ src/main/java/com/aplication/jetfeb/Controller/UsuarioController.java
@@ -1,9 +1,7 @@
 package com.aplication.jetfeb.Controller;
 
-import java.util.List;
-
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,32 +16,36 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioDao usuarioDao;
 
-	@RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
-	public Usuario getUsuario(@PathVariable Long id) {
-		Usuario usuario = new Usuario();
-		usuario.setIdUsuario(id);
-		usuario.setNombre("Maicol");
-		usuario.setApellido("Cifuentes");
-		usuario.setEmail("maicolpalabras@gmail.com");
-		usuario.setTelefono("3167556055");
-		usuario.setPassword("213421421");
-		return usuario;
-	}
+	// @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
+	// public Usuario getUsuario(@PathVariable Long id) {
+	// 	Usuario usuario = new Usuario();
+	// 	usuario.setIdUsuario(id);
+	// 	usuario.setNombre("Maicol");
+	// 	usuario.setApellido("Cifuentes");
+	// 	usuario.setEmail("maicolpalabras@gmail.com");
+	// 	usuario.setTelefono("3167556055");
+	// 	usuario.setPassword("213421421");
+	// 	return usuario;
+	// }
 
-	@RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
-	public List<Usuario> getUsuarios() {
-		return usuarioDao.getUsuarios();
+	// @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+	// public List<Usuario> getUsuarios() {
+	// 	return usuarioDao.getUsuarios();
 
-	}
+	// }
 
-	@RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
+	@RequestMapping(value = "api/usuarios/registro", method = RequestMethod.POST)
 	public void registrarUsuario(@RequestBody Usuario usuario) {
+
+		//Se genera el hash de la contrasena para guardar en la BD
+		usuario.setPassword(BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt()));
+
 		usuarioDao.registrar(usuario);
 	}
 
-	@RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
-	public void eliminar(@PathVariable Long id) {
-		usuarioDao.eliminar(id);
-	}
+	// @RequestMapping(value = "api/usuarios/eliminar/{id}", method = RequestMethod.DELETE)
+	// public void eliminar(@PathVariable Long id) {
+	// 	usuarioDao.eliminar(id);
+	// }
 
 }
